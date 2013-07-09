747a5d9572bea2282b60711b25b730470b0bfcac
Michael Lohmann <M.Lohmann@jacopo.de>
2013-07-09 20:28:32 +0200

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+console.log(fs.readFileSync('./index.html', 'utf-8'));
+//  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
